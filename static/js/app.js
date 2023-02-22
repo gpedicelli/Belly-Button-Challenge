a618e8f06a6ad8af2dcb2ee0e82b871a2fb83b15
Gabriele Pedicelli <gabepedicelli@yahoo.com>
2023-02-21 22:43:55 -0600

    Belly Button Challenge

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,8 @@
 //Get the samples
-​
+
 function buildCharts(sample){
   const samples = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
-​
+
   // Use `d3.json` to fetch the sample data for the plots
   d3.json(samples).then((data) => {
       var samples= data.samples;
@@ -12,7 +12,7 @@ function buildCharts(sample){
       var ids = result.otu_ids;
       var labels = result.otu_labels;
       var values = result.sample_values;
-​
+
       // Building bubble chart
       var LayoutBubble = {
           margin: { t: 0 },
@@ -59,21 +59,34 @@ function buildCharts(sample){
     
 function init() {
 // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
-​
+
+  const samples = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+  // Use `d3.json` to fetch the sample data for the plots
+  d3.json(samples).then((data) => {
+
+    var dropdownames=data.names;
+
+    // TA helped with this part 
+    var selector = d3.select("#selDataset");
+    for(let name = 0; name<dropdownames.length;name++){
+      selector.append("option").text(dropdownames[name]).property("value",dropdownames[name])
+    };
+  })
+
+    
   // Use the first sample from the list to build the initial plots
   // const firstSample = sampleNames[0];
   const firstSample = '940'
   buildCharts(firstSample);
   // buildMetadata(firstSample);
 }
-​
-​
+
 function optionChanged(newSample) {
 // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   // buildMetadata(newSample);
 }
-​
+
 // run init function on page load/refresh
 init()
